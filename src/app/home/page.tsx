7c2c3446497de7958efbc88ae24a71355c6c9fc2
nirmal uzamaki <kamura657@gmail.com>
2024-05-12 18:02:04 +0200

    Complete routing setup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>homePage</div>;
+};
+
+export default Home;
---
+++ src/app/projects/pages.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div>homePage</div>;
+};
+
+export default Projects;
