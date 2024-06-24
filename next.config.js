cc1e137f3db4010669880ff62c22b1d6b3b327c2
nirmal uzamaki <kamura657@gmail.com>
2024-06-25 00:49:08 +0200

    Set up GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+// next.config.mjs
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+export default {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  trailingSlash: true,
+}
