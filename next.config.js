f6fd7018e2f937387c0b975c96653216e2158584
nirmal uzamaki <kamura657@gmail.com>
2024-06-25 16:15:09 +0200

    update work flow file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   basePath: '/next2024',
   output: 'export',
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/welcome',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
