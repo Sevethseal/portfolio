5a206a1a97328956f839ff5131afb2ecb3588351
nirmal uzamaki <kamura657@gmail.com>
2024-06-25 15:53:27 +0200

    update work flow file

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/next2024',
   output: 'export',
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
